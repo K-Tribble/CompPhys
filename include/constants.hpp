f0b4f0b1b6851c47974092f6ee86a7604e03155e
Kai <krtribble7@icloud.com>
2026-08-17 00:04:46 +0100

    Changed some constants

--- include/constants.hpp
+++ include/constants.hpp
@@ -2,6 +2,6 @@
 
 #include "types.hpp"
 
-constexpr d64 kDefaultAbsTol = 1e-9;
-constexpr d64 kDefaultRelTol = 1e-9;
+constexpr d64 kDefaultAbsTol = 1e-10;
+constexpr d64 kDefaultRelTol = 1e-10;
 constexpr d64 kIterStopCondition = 1e-14;
