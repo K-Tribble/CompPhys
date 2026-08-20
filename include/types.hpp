6b2d0cff969c8d0cb9386c510f71725b8512c8bc
Kai <krtribble7@icloud.com>
2026-08-20 12:24:58 +0100

    Added aliases for complex float and double.

--- include/types.hpp
+++ include/types.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <cmath>
+#include <complex>
 
 using f32 = float;
 using d64 = double;
 using u32 = std::size_t;
+using c32 = std::complex<f32>;
+using c64 = std::complex<d64>;
